1359408f29eaddc4ea067e68479dc96b8fc51759
Meit Sant <97303250+MT-276@users.noreply.github.com>
2023-04-06 14:53:29 +0530

    Revert "Update Picture-Encoder-and-Decoder.py"
    This reverts commit 9dc26265dc7a0f7b96e338eccddd66565d054d75.

--- Picture-Encoder-and-Decoder.py
+++ Picture-Encoder-and-Decoder.py
@@ -15,50 +15,44 @@ option = input("Encode Image [E] or Decode string [D] :")
 Debug_mode = True
 
 Loaded = False
-c=0
 while Loaded != True:
     try:
-        from PIL import Image                                       #Importing third-party libraries
-        import os,sys,time,random
+        from PIL import Image
+        import os,sys,time
         Loaded = True
     except:
         print("\n[ERROR] An error occured while loading libraries\nAttempting to download libraries...")
         from os import system
-        system("pip install Pillow --user")                         #Attempting to download libraries if not found
-    if c == 2:
-        print("[ERROR] An error occured while loading libraries. Please Re-run the program")
-        sys.exit()
-    c+=1
+        system("pip install Pillow --user")
 
-#Todo1 Fix all Error messages to not be an infinite loop.
 
 #---------------------- Fuctions ----------------------
 
 def Encode(Nums):
     Nums = str(Nums)
-
-    if len(Nums) != 3:                                              # Checks if the provided data is a 3 digit no. or not
+    if len(Nums) != 3:
         p = 3-len(Nums)
         for v in range(p):
-            Nums+="N"                                               # If not then it adds 'N' which just signifies "none"
+            Nums+="N"
 
+    import random,os,sys,time
     Computed_nums_lst = []
     Computed_nums = ""
 
     #      0   1   2   3   4   5   6   7   8   9   N
-    l1 = ["a","b","c","d","e","f","g","h","i","j","x"]              # Key,Value table
+    l1 = ["a","b","c","d","e","f","g","h","i","j","x"]
     l2 = ["K","L","M","N","O","P","Q","R","S","T","Y"]
     l3 = ["!","@","#","$","%","^","&","*","-","~","="]
 
-    Key = random.randint(0,9)                                       # Selects a random Key
+    Encoder_value = random.randint(0,9)
 
     for i in Nums:
         if i != "N":
-            pointer = int(i)+Key                                    # Adds the key to the numeric value of the colour
+            pointer = int(i)+Encoder_value
             if pointer >= 10:
-                pointer = pointer - 10                              # If it is greater than 10, it loops around.
+                pointer = pointer - 10
 
-            Hash = random.randint(1,3)                              # Selecting one of the char lists
+            Hash = random.randint(1,3)
 
             if Hash == 1:
                 Computed_nums_lst.append(l1[pointer])
@@ -66,7 +60,7 @@ def Encode(Nums):
                 Computed_nums_lst.append(l2[pointer])
             if Hash == 3:
                 Computed_nums_lst.append(l3[pointer])
-        else:                                                       # Else condition where value of 'i' is "none"
+        else:
             Hash = random.randint(1,3)
             if Hash == 1:
                 Computed_nums_lst.append(l1[10])
@@ -78,23 +72,21 @@ def Encode(Nums):
     for j in Computed_nums_lst:
         Computed_nums += j
 
-    del Hash,pointer,Nums,Computed_nums_lst,l1,l2,l3,p,v
-
-    return Key,Computed_nums                                        # Returns Key and Encoded colour data
+    return Encoder_value,Computed_nums
 
 def Decode(Hashed_str):
 
     decoded = ""
 
     #      0   1   2   3   4   5   6   7   8   9   N
-    l1 = ["a","b","c","d","e","f","g","h","i","j","x"]              # Key-Value table
+    l1 = ["a","b","c","d","e","f","g","h","i","j","x"]
     l2 = ["K","L","M","N","O","P","Q","R","S","T","Y"]
     l3 = ["!","@","#","$","%","^","&","*","-","~","="]
 
-    Key = int(Hashed_str[0])                                        # Obtains the Key
-    Value = Hashed_str[1:4]                                         # Obtains the Value
+    Key = int(Hashed_str[0])
+    Value = Hashed_str[1:4]
 
-    for i in Value:                                                 # Checks index of the number in value
+    for i in Value:
         if i in l1:
             pointer = l1.index(i)
         if i in l2:
@@ -102,17 +94,15 @@ def Decode(Hashed_str):
         if i in l3:
             pointer = l3.index(i)
 
-        pointer -= Key                                              # Subtracts using key to get real number
+        pointer -= Key
 
         if pointer<0:
             pointer+=10
 
-        if i != "x" and i != "Y" and i != "=":                      # Checks for 'x','Y','=' which signify "none"
+        if i != "x" and i != "Y" and i != "=":
             decoded+=str(pointer)
 
-    del Key,Value,pointer,l1,l2,l3                                  # Deleting un-used variables to save RAM
-
-    return decoded                                                  # Returns decoded colour value
+    return decoded
 
 def Convert_to_JPG(path):
     F = "muk\muk"
@@ -123,24 +113,23 @@ def Convert_to_JPG(path):
     F = F[0]
     new_path = F+".jpg"
     im = Image.open(path)
-    im4 = im.convert('RGB')                                         # Converting to jpg format
-    im4.save(new_path)                                              # Saving temp jpg image
+    im4 = im.convert('RGB')
+    im4.save(new_path)
 
-    del im4                                                         # Deleting un-used variables to save RAM
+    del im4
     return new_path
 
 
-    #---------------------- Main ----------------------
+#---------------------- Main ----------------------
 
 
 if option == "E":
 
     #---------------------- Loading Image ----------------------
     Loaded = False
-    c = 0
     while Loaded != True:
         print()
-        Image_path = input("Enter path of the image : ")            # User input for path of Img.
+        Image_path = input("Enter path of the image : ")
 
         if '"' in Image_path:
             Image_path = Image_path.replace('"','')
@@ -154,30 +143,44 @@ if option == "E":
             F = F[1]
             Delete = False
             if F != "jpg":
-                Image_path = Convert_to_JPG(Image_path)             # This program works only on jpg files and hence
-                Delete = True                                       # will convert any non-jpg files to a temp jpg
-            im = Image.open(Image_path)                             # and will delete the temp jpg after execution
+                Image_path = Convert_to_JPG(Image_path)
+                Delete = True
+            im = Image.open(Image_path)
             start_time = time.perf_counter ()
             Loaded = True
         except:
             print("The path of the image is invalid, please try again!")
             if Debug_mode == True:
                 print("\n",Image_path)
-        if c == 5:
-            print("The path of the image is invalid")
-            sys.exit()
-        c+=1
 
-    pix = im.load()                                                 # Loading the image
-    m,n=im.size                                                     # Obtaining size of image
+    pix = im.load()
+    m,n=im.size
     Encoded =""
 
     del Loaded,F
-    #---------------------- Encoding ----------------------         # Start of Encoding process
-
-    print("\nEncoding image...")
+    #---------------------- Encoding ----------------------
 
     #ToDo3 Add "Estimated time" in non- debug mode
+
+    print("\nEncoding image...")
+    try:
+        for i in range(m):
+            for j in range(n):
+                tup = pix[i,j]
+                for k in tup:
+                    E,C = Encode(k)
+                    Encoded+=str(E)
+                    Encoded+=str(C)
+        Encoded+="."+str(m)+"?"+str(n)
+    #ToDo2 Save encoding to a txt file instead of saving in RAM.
+    except:
+        print("[ERROR] Encoding failed")
+        sys.exit()
+    del m,n,i,j,tup,k,E,C
+    print("Image Encoded")
+    #---------------------- Saving Encryption ----------------------
+
+    print("\nSaving Image Encoding...")
 
     try:
         F = "muk\muk"
@@ -186,35 +189,20 @@ if option == "E":
         F = F[-1]
         F = F.split(".")
         F = F[0]
-        F = "Encoded_"+F+".txt"                                     # Creates the file where the encoding is going
-        file = open(F, 'w')                                         # to be stored while encoding.
-    except:
-        print("[ERROR] Could not save encoding. Check Disk space")
-        sys.exit()
-
-
-    try:
-        for i in range(m):                                          # m = no. of rows
-            for j in range(n):                                      # n = no. of columns
-                tup = pix[i,j]                                      # pix[i,j] --> Gets the RGB data of the pixel
-                for k in tup:
-                    E,C = Encode(k)                                 # RGB value encoded via Encode() function
-                    Encoded+=str(E)
-                    Encoded+=str(C)
-                    file.write(Encoded)                             # [NEW] Directly saving to txt file instead of RAM.
-                    Encoded = ''
-
-        file.write("."+str(m)+"?"+str(n))
+        F = "Encoded_"+F+".txt"
+        file = open(F, 'w')
+        file.write(Encoded)
         file.close()
     except:
-        print("[ERROR] Encoding failed")
+        print("[ERROR] Encoded Data was not saved. Check Disk space")
         sys.exit()
-    del m,n,i,j,tup,k,E,C
-    print("Image Encoded")
 
-    if Delete == True:                                              # Checks if there was a temp JPG image created
-          os.remove(Image_path)                                     # in case the image was of a different format
-    del Delete,F,Encoded,file                                       # and deletes it.
+    print("Encoding saved successfully")
+
+    if Delete == True:
+          os.remove(Image_path)
+    del Delete,F,Encoded,file
+
 
 
 if option == "D":
@@ -224,90 +212,68 @@ if option == "D":
     Decoded_lst1 = []
     pixel_data = []
     Encoded_str = ''
+    Encoded_lst = []
+    c=1
 
     Loaded = False
-    c=0
     while Loaded != True:
         try:
-            Encoded_file_name = input("Enter Encoded file path : ") # User input for path of encoded file path
-
-            F = "muk\muk"
-            F = F.replace("muk","")
-            F = Encoded_file_name.split(F)
-            F = F[-1]
-            F = F.split(".")
-            F = F[-1]
-            if F != "txt":                                          # Checks if the file format is correct or not
-                print("[ERROR] File format incorrect")
-                sys.exit()
-
+            Encoded_file_name = input("Enter Encoded file path : ")
             with open(Encoded_file_name, 'r') as file:
-                Encoded_inp = file.read()                           # Loades file contents in a variable [Encoded_inp]
+                Encoded_inp = file.read()
             Loaded = True
-            start_time = time.perf_counter ()                       # Starts recording time for execution
+            start_time = time.perf_counter ()
 
         except:
             print("[ERROR] File path incorrect. Please try again")
             if Debug_mode == True:
                 print(Encoded_file_name)
-        if c == 5:
-            print("The path of the image is invalid")
-            sys.exit()
-        c+=1
 
-    del F,c,Loaded                                                  # Deleting un-used variables to save RAM
 
     #---------------------- Decoding ----------------------
     print("\nDecoding...")
-
-    c=1
     try:
-        pixel_encoding = Encoded_inp.split(".")                     # The encoding has two parts:
-#                                                                   #       (1) The pixel's encoding
-#                                                                   #       (2) The image's dimensions
-#                                                                   # The pixel's encoding and The image's dimensions
-#                                                                   # are separted by a "."
-
-
-        for Char in pixel_encoding[0]:                              # Seperates chunks of encoded pixel data
-            Encoded_str+=Char
+        e = Encoded_inp.split(".")
+        for o in e[0]:
+            Encoded_str+=o
             if c % 4 == 0:
-                Decoded_lst.append(Decode(Encoded_str))             # Decodes encoded pixel data
+                Encoded_lst.append(Encoded_str)
                 Encoded_str=''
             c+=1
 
+        for o in Encoded_lst:
+            Decoded_lst.append(Decode(o))
 
         c=1
-
-        for Char in Decoded_lst:
-            Decoded_lst1.append(Char)
+        x=0
+        y=0
+        for o in Decoded_lst:
+            Decoded_lst1.append(o)
             if c % 3 == 0:
                 R = int(Decoded_lst1[0])
                 G = int(Decoded_lst1[1])
                 B = int(Decoded_lst1[2])
                 #A = int(Decoded_lst1[3])
                 #print("(",R,",",G,",",B,",",A,")")
-                pixel_data.append((R,G,B))                          # Joins all the un-organised pixel data
+                pixel_data.append((R,G,B))
                 Decoded_lst1 = []
             c+=1
-
-        del Decoded_lst,Encoded_str                                 # Deleting un-used variables to save RAM
-
         Dimension_lst = Encoded_inp.split(".")[1]
         Dimension_lst = Dimension_lst.split("?")
-        m,n = int(Dimension_lst[0]),int(Dimension_lst[1])           # Gets dimension data from Encoded_inp
+        m,n = int(Dimension_lst[0]),int(Dimension_lst[1])
+        c=0
+        print("File Decoded\n\nInitializing image generation...")
     except:
         print("\n[ERROR] Decryption Failed. Please verify file contents")
         sys.exit()
 
-    del Encoded_inp                                                 # Deleting un-used variables to save RAM
+
 
     #---------------------- Converting into an image ----------------------
-    x,y=0,0
-    print("File Decoded\n\nInitializing image generation...")
-
     try:
-        image = Image.new('RGB', (m, n))                            # Creates a new image
+        image = Image.new('RGB', (m, n))
+        #print(m,"x",n)
+        #print(len(pixel_data))
 
         index = -1
         for x in range(m):
@@ -315,25 +281,23 @@ if option == "D":
                 index += 1
 
                 color = pixel_data[index]
-                image.putpixel((x, y), color)                       # Assigns colour to pixel using xy co-ordinate data
+                image.putpixel((x, y), color)
         print("Image Generated succesfully")
     except:
         print("\n[ERROR] Image Generation Failed")
         if Debug_mode == True:
             print("\n",m,"x",n,"\n",index,"\n",Encoded_file_name)
+
         sys.exit()
-
-
     #---------------------- Saving Image ----------------------
     print("\nSaving Image...")
-
     try:
-        image.save('Decoded_picture.jpg')                           # Saves the generated image in the same dir, where the program is being executed
+        image.save('Decoded_picture.jpg')
     except:
         print("[ERROR] Image was not saved. Check Disk space")
         sys.exit()
     print("Image saved succesfully")
-    os.startfile('Decoded_picture.jpg')                             # Opens the saved image
+    os.startfile('Decoded_picture.jpg')
 
 if option != 'E' and option != 'D':
     print("\n[ERROR] Please specify using 'E' and 'D' only")
@@ -343,4 +307,4 @@ if Debug_mode == True:
     ao = (end_time - start_time)//1
     if ao>=60:
         ao = str(ao//60) +" Min " + str(ao%60)
-    print("\nTime for execution : ",int(ao), "Sec")                 # Calculates and prints the time taken for execution of the program
+    print("\nTime for execution : ",int(ao), "Sec")
